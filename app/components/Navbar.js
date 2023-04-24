745a316e6efb3772c60fad0d44fb437ee6983c17
Jamey Huffnagle <mjhuffnagle@gmail.com>
2023-04-24 17:23:38 -0400

    search page

---
+++ app/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>Website Name</div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ app/components/SearchBarContainer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+
+const SearchBarContainer = ({ submittedURLHandler }) => {
+  return (
+    <div className="flex justify-center items-center w-screen animated animatedFadeInUp fadeInUp">
+      <div className="flex flex-col justify-center items-center w-screen mb-80">
+        <div className="flex flex-col items-center w-1/2">
+          <div className="bg-gray-700/[.9] p-6 w-full rounded-lg">
+            <div className="mr-auto text-8xl mb-3 cursor-default">
+              Enter A URL.
+            </div>
+            <SearchBar submittedURLHandler={submittedURLHandler} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchBarContainer;
