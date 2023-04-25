8ba35a6251b8f3cd1a27a8086799f31d5e25b86a
Mr Mercury <122291906+Mr-Mercury@users.noreply.github.com>
2023-04-25 15:20:14 -0400

    Added DataObj logic

--- app/api/sql-db/retrieve/[url]/route.js
+++ app/api/sql-db/retrieve/[url]/route.js
@@ -6,8 +6,8 @@ export async function GET(request) {
     const url = request.url.slice(request.url.lastIndexOf('/')+1);
     let finalUrl;
 
-    if (url.slice(0, 7) === 'http://') {
-        finalUrl = 'https://' + url.slice(8);
+    if (url.slice(0, 6) === 'http://') {
+        finalUrl = 'https://' + url.slice(7);
     } else if (url.slice(0, 8) !== 'https://') {
         finalUrl = 'https://' + url;
     } else {
@@ -21,6 +21,10 @@ export async function GET(request) {
         }
     })
 
+    if(searchedUrl) {
+        const searchedUrlObj = searchedUrl.dataObj
+    }
+
     // If it exists, prisma logic to return all info related to URL 
-    return NextResponse(searchedUrl)
+    return NextResponse(searchedUrlObj)
 }
--- app/api/sql-db/save/route.js
+++ app/api/sql-db/save/route.js
@@ -3,13 +3,16 @@ import { prisma } from "@/prisma";
 
 export async function POST(request) {
     // Deconstruct object - enter final vars on final object here 
-    const { url,  } = await request.json();
+    const { url, img } = await request.json();
+    const dataObj = await request.json();
+
 
     // Save to prisma DB according to save query 
     const newUrl = await prisma.baseurl.create({
         data: {
             url: url,
-            img: img
+            img: img,
+            dataObj: dataObj
         }
     })
 
