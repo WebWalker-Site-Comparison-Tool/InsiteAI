e7059119ad4bb16aac6ff744fe1f935c1787b9d6
Mr Mercury <122291906+Mr-Mercury@users.noreply.github.com>
2023-04-25 10:31:22 -0400

    Env key

---
+++ app/api/sql-db.js
@@ -0,0 +1,9 @@
+
+
+export default async function handler(req, res) {
+    if(req.method === 'GET') {
+        // prisma conditional for retrieving some data
+    } else if (req.method === 'POST') {
+        
+    }
+}
