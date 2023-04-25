8c0ba5c9343b9a0ecdddcd6e7ebcc16b53f8e38a
Jamey Huffnagle <mjhuffnagle@gmail.com>
2023-04-25 12:48:01 -0400

    more navbar updates

--- app/components/DataPageContainer.js
+++ app/components/DataPageContainer.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Navbar from './Navbar';
 
-const DataPageContainer = () => {
+const DataPageContainer = ({ URL }) => {
   return (
     <body className="bg-[url(https://www.pixel4k.com/wp-content/uploads/2018/10/color-waves-abstract_1539371252.jpg)]">
-      <Navbar />
+      <Navbar URL={URL} />
     </body>
   );
 };
--- app/components/SearchBarContainer.js
+++ app/components/SearchBarContainer.js
@@ -4,13 +4,14 @@ import SearchBar from './SearchBar';
 const SearchBarContainer = ({ submittedURLHandler, submittedURL }) => {
   return (
     <div
-      className={`flex justify-center items-center w-screen animated ${
+      className={`flex flex-col justify-center items-center w-screen animated ${
         !submittedURL ? 'FadeInUp fadeInUp' : `FadeOutUp fadeOutUp`
       }`}
     >
+      <img src="logo.png" className="w-[10%] mb-5" />
       <div className="flex flex-col justify-center items-center w-screen mb-80">
         <div className="flex flex-col items-center w-1/2">
-          <div className="bg-gray-700/[.9] p-6 w-full rounded-lg">
+          <div className="border-gray-200 dark:bg-gray-900/[.75] p-6 w-full rounded-lg">
             <div className="mr-auto text-8xl mb-3 cursor-default">
               Enter A URL.
             </div>
