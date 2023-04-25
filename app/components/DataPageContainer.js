de1f8a43464d18ab33f5d38bf230145359aef317
Jamey Huffnagle <mjhuffnagle@gmail.com>
2023-04-25 11:10:39 -0400

    added fetch to backend

---
+++ app/components/DataPageContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+const DataPageContainer = () => {
+  return (
+    <body className="bg-[url(https://www.pixel4k.com/wp-content/uploads/2018/10/color-waves-abstract_1539371252.jpg)]">
+      <Navbar />
+    </body>
+  );
+};
+
+export default DataPageContainer;
